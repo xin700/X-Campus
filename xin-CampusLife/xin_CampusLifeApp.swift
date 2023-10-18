466a12fd670edb7c5b95b8526ceb87a543e22e39
xin700 <992950082@qq.com>
2023-10-18 21:29:44 +0800

    Initial Commit

---
+++ xin-CampusLife/xin_CampusLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  xin_CampusLifeApp.swift
+//  xin-CampusLife
+//
+//  Created by xin on 2023/10/18.
+//
+
+import SwiftUI
+
+@main
+struct xin_CampusLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
